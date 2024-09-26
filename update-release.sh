eadc2aae22ce15f9482debd2820a806911c7d3cf
Austin L Mayes <me@austinlm.me>
2024-09-26 06:15:16 -0500

    Update spark-operator to version 2.0.0-27

---
+++ update-release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+OPERATOR=$1
+VERSION=$2
+BUILD_IMAGE=$3
+
+# default build image to false
+if [ -z "$BUILD_IMAGE" ]; then
+  BUILD_IMAGE=false
+else
+  BUILD_IMAGE=true
+fi
+
+SED_CMD='sed'
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  SED_CMD='gsed'
+fi
+
+cd "$(dirname "$0")"
+opm render quay.io/ziax/$OPERATOR-bundle:$VERSION -o yaml > catalog/$OPERATOR/$OPERATOR.v$VERSION.yaml
+cp catalog/$OPERATOR/$OPERATOR.yaml.template catalog/$OPERATOR/$OPERATOR.yaml
+$SED_CMD -i 's/$VERSION/'$VERSION'/g' catalog/$OPERATOR/$OPERATOR.yaml
+if [ "$BUILD_IMAGE" = true ]; then
+  docker build -f catalog.Containerfile -t quay.io/ziax/operator-catalog:latest . --platform=linux/amd64
+  docker push quay.io/ziax/operator-catalog:latest
+fi
+git add .
+git commit -m "Update $OPERATOR to version $VERSION"
+git push
